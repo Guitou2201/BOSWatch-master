de01e6b078caef0030295b976b7cc566b2e5d7f1
Arnaud <arnaud.gillou@gmail.com>
2024-12-22 21:39:04 +0100

    First commit

---
+++ includes/globalVars.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Global variables
+
+@author: Jens Herrmann
+@author: Bastian Schroll
+"""
+
+# version info
+versionNr = "2.5.2"
+branch = "master"
+buildDate = "08.01.2021"
+
+# Global variables
+config = 0
+script_path = ""
+log_path = ""
+
+# pluginLoader
+pluginList = {}
